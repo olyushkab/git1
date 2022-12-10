bf7b27d555c6f251ca547b695521a0514d54657f
olgaodarenko <olga12594@gmail.com>
2022-12-10 04:17:08 -0800

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+
+    //red files - unversioned files
+    //green files - versioned files
+    //VCS -> create GIT repository (select our project, but it should be selected by default)
+}
